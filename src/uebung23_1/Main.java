3fecbc888e408046680a1b71318b8846364494e9
Pentomatics <s.brehm1@gmx.de>
2018-08-22 16:57:30 +0200

    Fixed filepath problem

--- src/uebung23_1/Main.java
+++ src/uebung23_1/Main.java
@@ -1,13 +1,16 @@
 package uebung23_1;
 
+import java.lang.invoke.MethodHandles;
+
 public class Main {
 
-    private static final String PINS_FILE_NAME = "src/uebung23_1/pins.csv";
+    private static final String PINS_FILE_NAME = "pins.csv";
     private static final String PASSWORD_ALPHABET = "123456789abcdefghijklmnopqrstuvwxyz";
 
 
     public static void main(String[] args) {
-        PasswordGuessing pg = new PasswordGuessing(PINS_FILE_NAME, PASSWORD_ALPHABET);
+        String filename = MethodHandles.lookup().lookupClass().getResource(PINS_FILE_NAME).getPath();
+        PasswordGuessing pg = new PasswordGuessing(filename, PASSWORD_ALPHABET);
         pg.guessPasswords();
     }
 }
